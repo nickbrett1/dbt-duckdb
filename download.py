983503cf2f1c780fcf79ec145c641085872cfeac
Nick Brett <nick.brett1@gmail.com>
2025-04-26 01:34:49 +0000

    Now with rclone installed

---
+++ download.py
@@ -0,0 +1,4 @@
+# Download data from: https://databank.worldbank.org/data/download/WDI_CSV.zip
+# Use rclone to download the data? https://rclone.org/commands/rclone_copyurl/
+
+# Also see https://github.com/nickbrett1/data-science-on-gcp/blob/main/02_ingest/download.sh
