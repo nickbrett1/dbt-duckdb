673a8f500d0d6ab3abc3f825536c81f42375e0b4
Nick Brett <nick.brett1@gmail.com>
2025-05-31 15:59:14 +0000

    Change code to indicator_code

--- wdi/models/marts/dim_indicator.sql
+++ wdi/models/marts/dim_indicator.sql
@@ -1,11 +1,12 @@
-with source as (
-    select distinct
-         {{ adapter.quote("Series Code") }} as code,
-         {{ adapter.quote("Indicator Name") }} as indicator_name,
-         {{ adapter.quote("Topic") }} as topic
-    from {{ ref('stg_wdiseries') }}
-    where lower({{ adapter.quote("Topic") }}) like '%economic%'
-)
+with
+    source as (
+        select distinct
+            {{ adapter.quote("Series Code") }} as indicator_code,
+            {{ adapter.quote("Indicator Name") }} as indicator_name,
+            {{ adapter.quote("Topic") }} as topic
+        from {{ ref('stg_wdiseries') }}
+        where lower({{ adapter.quote("Topic") }}) like '%economic%'
+    )
 
 select *
 from source
